7f85856e462352a26524c6823c46dbfbe6955f8c
Ashab <ashabuddin54321@gmail.com>
2024-01-06 07:19:37 +0600

    typeScript

---
+++ buildInDataType/index.ts
@@ -0,0 +1,7 @@
+// Any (super type)
+// built in types: number, string, boolean, void, null, undefined, never
+// user-defined types: Arrays, Enums, Classes, interfaces etc.
+
+// number: Represents both integer and floating-point numbers.
+let age: number = 25;
+let price: number = 9.99;
---
+++ index.ts
@@ -0,0 +1,9 @@
+// let userName="Ashab"
+// console.log(userName);
+// let age=21
+// console.log(age);
+
+function addNumber(num1:number, num2:number) {
+  console.log(num1 + num2);
+}
+addNumber(3, 2);
